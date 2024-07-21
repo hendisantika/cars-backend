c238f4b519fc091c568e4cc1c06738e604bac6a5
hendisantika <hendisantika@yahoo.co.id>
2024-07-21 08:50:24 +0700

    Add CarSearchParametersUtil🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/carsbackend/util/CarSearchParametersUtil.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.carsbackend.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cars-backend
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/07/24
+ * Time: 08.50
+ * To change this template use File | Settings | File Templates.
+ */
+public class CarSearchParametersUtil {
+}

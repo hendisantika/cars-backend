2a906d8b4b6d9fdfefafe66ed07013235a200c94
hendisantika <hendisantika@yahoo.co.id>
2024-07-21 08:51:39 +0700

    Add Range🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/carsbackend/util/CarSearchParametersUtil.java
+++ src/main/java/id/my/hendisantika/carsbackend/util/CarSearchParametersUtil.java
@@ -82,4 +82,23 @@ public class CarSearchParametersUtil {
         }
 
     }
+
+    public static class Range {
+
+        private final int start;
+        private final int end;
+
+        public Range(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
+    }
 }

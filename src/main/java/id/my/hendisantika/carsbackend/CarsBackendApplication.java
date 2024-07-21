bcce29e1335313144839cd3e49613cb350c1fa53
hendisantika <hendisantika@yahoo.co.id>
2024-07-21 08:30:54 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/carsbackend/CarsBackendApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.carsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/carsbackend/CarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.carsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

8e7d008f3464c1d8f1d0ac3e518f6a7853ad785a
hendisantika <hendisantika@yahoo.co.id>
2024-07-21 08:47:35 +0700

    Add ResponseStatus Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/carsbackend/model/ResponseStatus.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.carsbackend.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : cars-backend
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/07/24
+ * Time: 08.47
+ * To change this template use File | Settings | File Templates.
+ */
+public class ResponseStatus {
+    public static final String SUCCESS = "success";
+    public static final String FAIL = "fail";
+    public static final String ERROR = "error";
+    public static final String NOT_FOUND = "not_found";
+    public static final String UNAUTHORIZED = "unauthorized";
+    public static final String INVALID_PARAMETERS = "invalid_parameters";
+    public static final String DATABASE_ERROR = "database_error";
+    public static final String SERVER_ERROR = "server_error";
+}
